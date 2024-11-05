3ec745cbd8d4d248657081fd78f214a7cba3fa2e
Luis Afonso <luis.manuel.afonso@ipvc.pt>
2024-11-05 13:56:38 +0000

    PDSDI

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import React from 'react';
 import { createRoot } from 'react-dom/client'
 import { App } from '../src/app/app'
-import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
